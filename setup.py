a06d6ea1094480aa03c77cfa22748ee194d4b68b
Michael Pasternak <mpastern@redhat.com>
2011-11-24 00:00:48 +0200

    sdk: add egg-info
    Change-Id: I6c6cd24e38f62b77f2cd7e97479632479bdc4c88

---
+++ setup.py
@@ -0,0 +1,34 @@
+
+import os
+import sys
+
+from distutils.command.build import build
+from setuptools import setup, Command
+
+
+version_info = {
+    'name': 'ovirt-engine-sdk',
+    'version': '1.0-SNAPSHOT',
+    'description': 'A SDK interface to oVirt'
+                   ' Virtualization',
+    'author': 'Unknown',
+    'author_email': 'engine-devel@ovirt.org',
+    'url': 'www.ovirt.org',
+    'license': 'ASL2',
+    'classifiers': [
+        'Development Status :: 1 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: ASL2 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.4' ],
+}
+
+
+setup(
+    package_dir={ '': 'src' },
+    packages=[ 'ovirtsdk.infrastructure', 'ovirtsdk.utils', 'ovirtsdk.web', 'ovirtsdk.xml'],
+    install_requires=[],
+    entry_points={},
+    **version_info
+)
