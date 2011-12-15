571864955cb72744889681ffeaa4c1ecd95f0260
Michael Pasternak <mpastern@redhat.com>
2011-12-15 16:07:32 +0200

    sdk: set right Python version
    Change-Id: I9cf13aeb6141c5f64e67298c915f02ce33b5fb51

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ version_info = {
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: ASL2 License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.4' ],
+        'Programming Language :: Python :: 2.7' ],
 }
 
 
