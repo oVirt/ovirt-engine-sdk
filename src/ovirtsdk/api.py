a3cb4b42be438998fa190b957da03d3c462deb8c
Michael Pasternak <mpastern@redhat.com>
2012-01-08 12:22:25 +0200

    sdk: add ASL2.0 license
    Change-Id: Ieacedc7351552b885def0d86d248444e57aacf26

--- src/ovirtsdk/api.py
+++ src/ovirtsdk/api.py
@@ -1,3 +1,18 @@
+#
+# Copyright (c) 2010 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#           http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 
 ########################################
@@ -5,7 +20,7 @@
 ########################################
 
 '''
-Generated at: 2011-11-23 16:14:52.640702
+Generated at: 2012-01-08 12:18:35.056597
 
 @author: mpastern@redhat.com
 '''
--- src/ovirtsdk/infrastructure/brokers.py
+++ src/ovirtsdk/infrastructure/brokers.py
@@ -1,3 +1,18 @@
+#
+# Copyright (c) 2010 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#           http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 
 ########################################
@@ -5,7 +20,7 @@
 ########################################
 
 '''
-Generated at: 2012-01-01 14:38:51.670589
+Generated at: 2012-01-08 12:18:33.519309
 
 @author: mpastern@redhat.com
 '''
