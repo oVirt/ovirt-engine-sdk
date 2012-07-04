8e7feda264fd6e5f4ea2b69c5d7f2bb12aa46755
Michael Pasternak <mpastern@redhat.com>
2012-07-04 10:46:17 +0300

    sdk: extract error body <h1>xxx</h1> from detail produced by application server #827881
    https://bugzilla.redhat.com/show_bug.cgi?id=827881
    
    Change-Id: Ida7ca2cda5c452dc6e83c65fbb484aa87f2709ad
    Signed-off-by: Michael Pasternak <mpastern@redhat.com>

--- src/ovirtsdk/infrastructure/errors.py
+++ src/ovirtsdk/infrastructure/errors.py
@@ -93,19 +93,29 @@ class RequestError(Exception):
         detail = ''
 
         if res is not None and str(res) is not '' and str(res).find('Error report') != -1:
-            detail = res
+            detail = res.lstrip()
         elif res:
             f_detail = params.parseString(res)
             if isinstance(f_detail, params.Action) and f_detail.fault is not None:
                 #self.reason = f_detail.fault.reason
-                detail = f_detail.fault.detail
+                detail = f_detail.fault.detail.lstrip()
             else:
                 #self.reason = response.reason
                 if f_detail is not None:
-                    detail = f_detail.detail
+                    detail = f_detail.detail.lstrip()
 
+        #engine returns can-do-action error messages with brackets
         if detail.startswith('[') and detail.endswith(']'):
             detail = detail[1:len(detail) - 1]
+
+        #application server error
+        if detail.startswith('<html>'):
+            start = detail.find('<h1>')
+            end = detail.find('</h1>')
+            if start != -1 and end != -1:
+                detail = detail[start:end].replace('<h1>', '').replace('</h1>', '')
+                if detail and detail.endswith(' - '):
+                    detail = detail[:len(detail) - 3]
 
         self.detail = detail
         self.reason = response.reason
