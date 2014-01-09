baccf389df6fe353240971a1d0fdadce7fd8028a
Juan Hernandez <juan.hernandez@redhat.com>
2014-01-09 20:19:27 +0100

    sdk: Release 3.4.0.2
    Change-Id: I4b9d37ad2d315562d54efc2b8d875baaf7b4e67f
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Command
 
 version_info = {
     'name': 'ovirt-engine-sdk-python',
-    'version': '3.4.0.1-1',
+    'version': '3.4.0.2',
     'description': 'A SDK interface to oVirt Virtualization',
     'author': 'Michael Pasternak',
     'author_email': 'mpastern@redhat.com',
