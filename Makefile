40ff62ba968615c7e121f939bfcb7c81b3a7259a
Juan Hernandez <juan.hernandez@redhat.com>
2015-03-18 10:49:59 +0100

    sdk: Prepare for next development iteration
    In preparation for the next development iteration this patch
    increases the version number and sets the RPM relase number to 0.1.
    
    Change-Id: I33159f1a7712590956bb80719cc43fc3856ee6be
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: rpm
 
-rpmrelease:=1$(rpmsuffix)
-rpmversion=3.5.2.1
+rpmrelease:=0.1$(rpmsuffix)
+rpmversion=3.5.2.2
 RPMTOP=$(shell bash -c "pwd -P")/rpmtop
 SPEC=ovirt-engine-sdk-python.spec
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Command
 
 version_info = {
     'name': 'ovirt-engine-sdk-python',
-    'version': '3.5.2.1',
+    'version': '3.5.2.2',
     'description': 'A SDK interface to oVirt Virtualization',
     'author': 'Michael Pasternak',
     'author_email': 'mpastern@redhat.com',
