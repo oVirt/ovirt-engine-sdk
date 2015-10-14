117764a8c1e62219f08f1975a2f823604d61a8aa
Juan Hernandez <juan.hernandez@redhat.com>
2015-10-14 10:24:21 +0200

    sdk: Prepare for next development iteration
    In preparation for the next development iteration this patch
    increases the version number and sets the RPM relase number to 0.1.
    
    Change-Id: I10a482574388693652113422d20d40629ad0321c
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Command
 
 version_info = {
     'name': 'ovirt-engine-sdk-python',
-    'version': '3.6.0.3',
+    'version': '3.6.0.4',
     'description': 'A SDK interface to oVirt Virtualization',
     'author': 'Michael Pasternak',
     'author_email': 'mpastern@redhat.com',
