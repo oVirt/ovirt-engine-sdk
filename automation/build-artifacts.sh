d9825909352633508f3ebbeb605ef291f2ddd994
Ondra Machacek <omachace@redhat.com>
2016-06-02 18:53:34 +0200

    Release 4.0.0.a3
    Change-Id: I4b0226a2c9f91c715ed12c881b639b7adcc81573
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -44,13 +44,15 @@ tar_file="${PWD}/sdk/dist/ovirt-engine-sdk-${version}.tar.gz"
 # Build the RPM:
 date="$(date --utc +%Y%m%d)"
 commit="$(git log -1 --pretty=format:%h)"
-suffix=".${date}git${commit}"
+#suffix=".${date}git${commit}"
+suffix=".a3"
 cp "${tar_file}" packaging/.
 pushd packaging
   export tar_version="${version}"
   export tar_url="$(basename ${tar_file})"
   export rpm_dist="$(rpm --eval '%dist')"
-  export rpm_release="0.0${suffix}${rpm_dist}"
+  #export rpm_release="0.0${suffix}${rpm_dist}"
+  export rpm_release="0.3${suffix}${rpm_dist}"
   ./build.sh
 popd
 
