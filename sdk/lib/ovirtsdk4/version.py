eff1a0fbeb6174ba512e71d9eb706e5faf08c75f
Ondra Machacek <omachace@redhat.com>
2016-07-19 13:21:41 +0200

    Release 4.0.0.a5
    Change-Id: I349ef5680aa240a7ab819a414d63885945465bcc
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- sdk/lib/ovirtsdk4/version.py
+++ sdk/lib/ovirtsdk4/version.py
@@ -17,7 +17,7 @@
 #
 
 # The version of the SDK:
-VERSION = '4.0.0a4'
+VERSION = '4.0.0a5'
 
 # This module is executed as a script during the build process, and it
 # should print to the output the value of the version:
