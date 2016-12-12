1417b18b4ca74b2400a3910a0afbdff2bd4258ec
Ondra Machacek <omachace@redhat.com>
2016-12-12 17:15:08 +0100

    Add examples of how to work with tags
    This patch adds an examples of how to work with tags.
    
    Change-Id: Ied84d6e82210f81c891cfc03a2328060b43f08d7
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

---
+++ sdk/examples/list_tags_of_vm.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import logging
+
+import ovirtsdk4 as sdk
+
+logging.basicConfig(level=logging.DEBUG, filename='example.log')
+
+# This example will connect to the server and list all tags of the virtual machine:
+
+# Create the connection to the server:
+connection = sdk.Connection(
+    url='https://engine40.example.com/ovirt-engine/api',
+    username='admin@internal',
+    password='redhat123',
+    ca_file='ca.pem',
+    debug=True,
+    log=logging.getLogger(),
+)
+
+# Get the reference to the "vms" service:
+vms_service = connection.system_service().vms_service()
+
+# Find the virtual machine:
+vm = vms_service.list(search='name=myvm0')[0]
+
+# Find the service that manages the vm:
+vm_service = vms_service.vm_service(vm.id)
+
+# Locate the service that manages the tags of the vm:
+tags_service = vm_service.tags_service()
+
+# Print all name and description of tags assigned to virtual machine:
+for tag in tags_service.list():
+    print("%s: %s" % (tag.name, tag.description))
+
+# Close the connection to the server:
+connection.close()
+
