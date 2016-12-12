de6551d98c5e5228be2595c4518c4c26a58a8ba5
Ondra Machacek <omachace@redhat.com>
2016-12-12 17:15:08 +0100

    Add examples of how to work with tags
    This patch adds an examples of how to work with tags.
    
    Change-Id: Ied84d6e82210f81c891cfc03a2328060b43f08d7
    Signed-off-by: Ondra Machacek <omachace@redhat.com>
    (cherry picked from commit 1417b18b4ca74b2400a3910a0afbdff2bd4258ec)

---
+++ sdk/examples/remove_tag.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (c) 2016 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import logging
+
+import ovirtsdk4 as sdk
+
+logging.basicConfig(level=logging.DEBUG, filename='example.log')
+
+# This example will connect to the server, search for a tag by name and
+# remove it:
+
+# Create the connection to the server:
+connection = sdk.Connection(
+    url='https://engine40.example.com/ovirt-engine/api',
+    username='admin@internal',
+    password='redhat123',
+    ca_file='ca.pem',
+    debug=True,
+    log=logging.getLogger(),
+)
+
+# Find the service that manages tags:
+tags_service = connection.system_service().tags_service()
+
+# Find the tag:
+tag = next(tag for tag in tags_service.list() if tag.name == 'mytag')
+
+# Find the service that manages the tag:
+tag_service = tags_service.tag_service(tag.id)
+
+# Remove the tag:
+tag_service.remove()
+
+# Close the connection to the server:
+connection.close()
