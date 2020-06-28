3c8e47256001922722accb8b7611aaa43c604b1a
Eyal Shenitzky <eshenitz@redhat.com>
2020-06-28 23:55:08 +0300

    examples: Add remove checkpoint example
    Add remove_checkpoint.py example to remove VM root checkpoint.
    
    ./remove_checkpoint.py -h
    usage: remove_checkpoint.py [-h] --engine-url ENGINE_URL --username USERNAME
                                [--password-file PASSWORD_FILE] [-c CAFILE]
                                [--insecure] [--debug]
                                vm_uuid
    
    Remove VM checkpoint
    
    positional arguments:
      vm_uuid               UUID of the checkpoint VM
    
    optional arguments:
      -h, --help            show this help message and exit
      --engine-url ENGINE_URL
                            oVirt engine URL (e.g. https://engine_fqdn:port)
      --username USERNAME   username of engine API
      --password-file PASSWORD_FILE
                            file containing password of the specified by user (if
                            file is not specified, read from standard input)
      -c CAFILE, --cafile CAFILE
                            path to oVirt engine certificate for verifying server.
      --insecure            do not verify server certificates and host name (not
                            recommended).
      --debug               log debug level messages to log file specified in
                            logger
    
    Change-Id: I296b9a139cf51be36869d080eeaeb95f65f0807f
    Signed-off-by: Eyal Shenitzky <eshenitz@redhat.com>

---
+++ sdk/examples/remove_checkpoint.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (c) 2020 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+This example will connect to the server and perform removal of a
+VM checkpoint.
+
+Note that this feature API is supported from version 4.4
+but the feature is currently in tech-preview until libvirt will
+release official release that contains the support for incremental
+backup.
+"""
+import argparse
+import time
+
+from contextlib import closing
+
+import ovirtsdk4 as sdk
+from helpers import common
+from helpers.common import progress
+
+common.configure_logging()
+
+parser = argparse.ArgumentParser(description="Remove VM checkpoint")
+common.add_engine_arguments(parser)
+parser.add_argument("vm_uuid", help="UUID of the checkpoint VM")
+args = parser.parse_args()
+
+progress("Removing root checkpoint for VM %r" % args.vm_uuid)
+
+# Create a connection to the server
+connection = common.create_connection(args)
+with closing(connection):
+    progress("Looking up checkpoints %s" % args.vm_uuid)
+    system_service = connection.system_service()
+    vm_service = system_service.vms_service().vm_service(id=args.vm_uuid)
+    checkpoints_service = vm_service.checkpoints_service()
+
+    # Validate that the VM has checkpoints
+    checkpoints = checkpoints_service.list()
+    if not checkpoints:
+        raise RuntimeError("VM {} has no checkpoints".format(args.vm_uuid))
+
+    # Get the first checkpoint in the chain
+    root_checkpoint = checkpoints_service.list()[0]
+    progress("Removing root checkpoint %r" % root_checkpoint.id)
+
+    # Removing the root checkpoint
+    checkpoint_service = checkpoints_service.checkpoint_service(id=root_checkpoint.id)
+    checkpoint_service.remove()
+
+    # Validate that the checkpoint removed
+    try:
+        while checkpoint_service.get():
+            time.sleep(1)
+    except sdk.NotFoundError:
+        progress("Root checkpoint removed successfully")
