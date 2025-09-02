db4d9e1afe27fd0f784fa26026c0dba7791fe917
Jasper Berton <jasper.berton@team.blue>
2025-09-02 10:28:56 +0200

    legal: fixed copyright files
    Altered headers on all files to reference the ovirt authors and not Redhat.
    
    Signed-off-by: Jasper Berton <jasper.berton@team.blue>

--- sdk/tests/test_iscsi_discover.py
+++ sdk/tests/test_iscsi_discover.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Copyright (c) 2016 Red Hat, Inc.
+# Copyright oVirt Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
